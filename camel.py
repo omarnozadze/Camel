f9571b82acc2dcf2c06df62afe38bd806810acd5
omarnozadze <127139782+omarnozadze@users.noreply.github.com>
2023-03-14 17:28:03 +0400

    Add files via upload

---
+++ camel.py
@@ -0,0 +1,8 @@
+camelCase= input("camelCase:"" ")
+camelCase = camelCase[0].lower() + camelCase[1:]
+
+for i in camelCase:
+    if i.isupper():
+        camelCase=camelCase.replace(i,f"_{i.lower()}")
+
+print(camelCase)        
